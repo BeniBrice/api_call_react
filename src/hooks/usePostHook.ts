74dcf168ccb5274103a9d39bf1aad8aa9c1fcffa
Beni Brice <benibrice2@gmail.com>
2025-09-10 17:42:29 +0200

    fix_bug

--- src/hooks/usePostHook.ts
+++ src/hooks/usePostHook.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { Post } from "../models/postModel";
 import postList from "../api/postApi";
 
@@ -19,22 +19,52 @@ function usePostHook({ userId, currentPage }: props): userPostsResults {
   let [posts, setPosts] = useState<Post[]>([]);
   let [hasError, setHasError] = useState<boolean>(false);
   let [totalPage, setTotalPages] = useState<number>(0);
-
+  const cache = useRef<Record<string, { posts: Post[]; totalPages: number }>>(
+    {}
+  );
   useEffect(() => {
-    setIsLoading(true);
-    postList({ currentPage: currentPage, userId: userId })
-      .then((data) => {
-        let { posts, totalPages } = data;
+    const cacheKey = `${currentPage}-${userId ?? "all"}`;
+    // setIsLoading(true);
+    if (cache.current[cacheKey]) {
+      setPosts(cache.current[cacheKey].posts);
+      setTotalPages(cache.current[cacheKey].totalPages);
+      console.log("DATA IS ARLEADY I CACHE");
+      return;
+    }
+
+    async function fetchData(): Promise<void> {
+      setIsLoading(true);
+      setHasError(false);
+      try {
+        let { posts, totalPages } = await postList({
+          userId: userId,
+          currentPage: currentPage,
+        });
+        cache.current[cacheKey] = { posts, totalPages };
+
         setPosts(posts);
         setTotalPages(totalPages);
-      })
-      .catch((e) => {
-        console.log("Error WHILE GETTING USER POST", e);
+      } catch (error) {
         setHasError(true);
-      })
-      .finally(() => {
+      } finally {
         setIsLoading(false);
-      });
+      }
+    }
+
+    fetchData();
+    // postList({ currentPage: currentPage, userId: userId })
+    //   .then((data) => {
+    //     let { posts, totalPages } = data;
+    //     setPosts(posts);
+    //     setTotalPages(totalPages);
+    //   })
+    //   .catch((e) => {
+    //     console.log("Error WHILE GETTING USER POST", e);
+    //     setHasError(true);
+    //   })
+    //   .finally(() => {
+    //     setIsLoading(false);
+    //   });
   }, [currentPage, userId]);
   return { isLoading, posts, hasError, totalPage };
 }
--- src/sections/tab_bar_section.tsx
+++ src/sections/tab_bar_section.tsx
@@ -1,5 +1,7 @@
 import { createContext, useState } from "react";
 import TabBarText, { tabsContent, type Tabs } from "../components/tab_bar_text";
+import DisplayUserSection from "./display_user_section";
+import PostSection from "./post_section";
 
 export let TabBarContext = createContext<Tabs | null>(null);
 
@@ -9,7 +11,14 @@ function TabBarSection() {
     <TabBarContext.Provider value={currentTab}>
       <section className="w-full flex flex-col items-center justify-center pt-10">
         <TabBarText setTab={setTab} />
-        <div className="p-10">{currentTab.tabContent}</div>
+        <div className="p-10">
+          <div className={currentTab.tabNumber === 0 ? "block" : "hidden"}>
+            <DisplayUserSection />
+          </div>
+          <div className={currentTab.tabNumber === 1 ? "block" : "hidden"}>
+            <PostSection />
+          </div>
+        </div>
       </section>
     </TabBarContext.Provider>
   );
